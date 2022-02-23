8f0064ebe1d9c915ea624b79427d1b99ddb6046f
theselfobsessedgeek <shubhamahlawatg@gmail.com>
2022-02-23 14:12:39 +0530

    changed the judges data to a map

--- lib/data/judgeData.dart
+++ lib/data/judgeData.dart
@@ -6,18 +6,73 @@ List<Map<String, String>> judgeData = [
     "designation": "Founder, Fun2Do Labs\nEx - VP, PayTM",
     "category": "Speaker - FinTech",
     "linkedIn": "https://www.linkedin.com/in/saurabhskj/",
-  }
+  },
+  {
+    "name": "Mr. Rakshit Tandon",
+    "image":
+        "https://firebasestorage.googleapis.com/v0/b/vihaan2021-b1b86.appspot.com/o/images%2Fjudges%2Fspeaker_rakshittandon.jpg?alt=media&token=8c329811-53ac-432f-bd19-cfbf3e7fe764",
+    "designation": "Founder, Fun2Do Labs\nEx - VP, PayTM",
+  },
+  {
+    "name": 'Mr. Sumeet Malik',
+    "image":
+        "https://firebasestorage.googleapis.com/v0/b/vihaan2021-b1b86.appspot.com/o/images%2Fjudges%2Fspeaker_sumeetmalik.jpg?alt=media&token=ccbe0689-f699-494e-8261-f7bd97a31494",
+    "designation": "Founder at Pepcoding Education Private Limited"
+  },
+  {
+    "name ": "Mr. Alon Grinshpoon",
+    "image":
+        'https://firebasestorage.googleapis.com/v0/b/vihaan2021-b1b86.appspot.com/o/images%2Fjudges%2Fspeaker_alongrinshpoon.jpg?alt=media&token=79668171-da03-4fb0-be6d-41ecb40eb39e',
+    "designation": 'Founder & CEO at echoAR (Techstars \'19)',
+  },
+  {
+    "name ": 'Mr. Arnav Gupta',
+    "image":
+        'https://firebasestorage.googleapis.com/v0/b/vihaan2021-b1b86.appspot.com/o/images%2Fjudges%2Fspeaker_arnavgupta.jpg?alt=media&token=136a8fb6-6c94-49b3-b2b1-a47e21b48ec4',
+    "designation":
+        'Founding Member - Coding Blocks\nLead Engineer, Android - Target',
+  },
+  {
+    "name ": 'Mr. Shivaam Sharma',
+    "image":
+        'https://firebasestorage.googleapis.com/v0/b/vihaan2021-b1b86.appspot.com/o/images%2Fjudges%2Fspeaker_shivaamsharma.jpg?alt=media&token=733340bb-6512-4f6b-b24d-f34018e14b05',
+    "designation": 'Founder and CEO, Trans Neuron Technologies',
+  },
+  {
+    "name ": 'Mr. Utkarsh Singh',
+    "image":
+        'https://firebasestorage.googleapis.com/v0/b/vihaan2021-b1b86.appspot.com/o/images%2Fjudges%2Fjudge_utkarshsingh.jpg?alt=media&token=73ead81f-5886-454d-80d3-36df237be876',
+    "designation": 'Senior Hardware Engineer, Samsung Electronics',
+  },
+  {
+    "name ": 'Ms. Drishti Agarwal',
+    "image":
+        'https://firebasestorage.googleapis.com/v0/b/vihaan2021-b1b86.appspot.com/o/images%2Fjudges%2Fjudge_drishti.jpg?alt=media&token=0d04007c-9cdb-4781-bd30-8219e24f2f88',
+    "designation": 'SDE at Adobe Systems',
+  },
+  {
+    "name ": 'Ms. Ishika Hooda',
+    "image":
+        'https://firebasestorage.googleapis.com/v0/b/vihaan2021-b1b86.appspot.com/o/images%2Fjudges%2Fjudge_ishika.jpg?alt=media&token=a4d364a0-ebcb-4617-921e-1fb98476d3b4',
+    "designation": 'Software Developer, Atlassian',
+  },
+  {
+    "name ": 'Mr. Samarth Gupta',
+    "image":
+        'https://firebasestorage.googleapis.com/v0/b/vihaan2021-b1b86.appspot.com/o/images%2Fjudges%2Fjudge_samarth-1.jpg?alt=media&token=9bebe1f5-0540-444a-936e-c22d6d6896b3',
+    "designation": 'Software Engineer, Microsoft\nIEEE REM Scholar',
+  },
 ];
 
 // var judgesName = [
-//   'Mr. Saurabh Jain',
-//   'Mr. Rakshit Tandon',
-//   'Mr. Sumeet Malik',
-//   'Mr. Alon Grinshpoon',
-//   'Mr. Arnav Gupta',
-//   'Mr. Shivaam Sharma',
-//   'Mr. Utkarsh Singh',
-//   'Mr. Samarth Gupta',
+//   'Mr. Saurabh Jain',  DONE
+//   'Mr. Rakshit Tandon',  DONE
+//   'Mr. Sumeet Malik',  DONE
+//   'Mr. Alon Grinshpoon', DONE
+//   'Mr. Arnav Gupta',   DONE
+//   'Mr. Shivaam Sharma',  DONE
+//   'Mr. Utkarsh Singh',     DONE
+//   'Mr. Samarth Gupta',    DONE
 //   'Ms. Ishika Hooda',
 //   'Ms. Drishti Agarwal',
 //   //'Mr. Ritwik Ranjan',
@@ -34,9 +89,9 @@ List<Map<String, String>> judgeData = [
 //   'Mr. Arnav Gupta': 'Founding Member - Coding Blocks',
 //   'Mr. Shivaam Sharma': 'Founder and CEO, Trans Neuron Technologies',
 //   'Mr. Utkarsh Singh': 'Senior Hardware Engineer, Samsung Electronics',
-//   'Mr. Samarth Gupta': 'Software Engineer, Microsoft\nIEEE REM Scholar',
-//   'Ms. Ishika Hooda': 'Software Developer, Atlassian',
-//   'Ms. Drishti Agarwal': 'SDE at Adobe Systems',
+//   'Mr. Samarth Gupta': 
+//   'Ms. Ishika Hooda': 
+//   'Ms. Drishti Agarwal': 
 // };
 //
 // var judgesLinkedIn = {
