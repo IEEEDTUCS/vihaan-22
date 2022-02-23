4492729ae7e7895dff429b2eaeb645017a1b39bf
theselfobsessedgeek <shubhamahlawatg@gmail.com>
2022-02-23 14:42:17 +0530

    Fixed oveflow bug in judge screen

--- lib/data/judgeData.dart
+++ lib/data/judgeData.dart
@@ -12,112 +12,71 @@ List<Map<String, String>> judgeData = [
     "image":
         "https://firebasestorage.googleapis.com/v0/b/vihaan2021-b1b86.appspot.com/o/images%2Fjudges%2Fspeaker_rakshittandon.jpg?alt=media&token=8c329811-53ac-432f-bd19-cfbf3e7fe764",
     "designation": "Founder, Fun2Do Labs\nEx - VP, PayTM",
+    "category": "Speaker - Security",
+    "linkedIn": "https://www.linkedin.com/in/rakshittandon/",
   },
   {
-    "name": 'Mr. Sumeet Malik',
+    "name": "Mr. Sumeet Malik",
     "image":
         "https://firebasestorage.googleapis.com/v0/b/vihaan2021-b1b86.appspot.com/o/images%2Fjudges%2Fspeaker_sumeetmalik.jpg?alt=media&token=ccbe0689-f699-494e-8261-f7bd97a31494",
-    "designation": "Founder at Pepcoding Education Private Limited"
+    "designation": "Founder at Pepcoding Education Private Limited",
+    "category": "Speaker - Education",
+    "linkedIn": "https://www.linkedin.com/in/sumeet-malik-b9a8b9b6/",
   },
   {
-    "name ": "Mr. Alon Grinshpoon",
+    "name": "Mr. Alon Grinshpoon",
     "image":
         'https://firebasestorage.googleapis.com/v0/b/vihaan2021-b1b86.appspot.com/o/images%2Fjudges%2Fspeaker_alongrinshpoon.jpg?alt=media&token=79668171-da03-4fb0-be6d-41ecb40eb39e',
     "designation": 'Founder & CEO at echoAR (Techstars \'19)',
+    "category": "Speaker - AR/VR",
+    "linkedIn": "https://www.linkedin.com/in/alon-grinshpoon-b9a8b9b6/",
   },
   {
-    "name ": 'Mr. Arnav Gupta',
+    "name": 'Mr. Arnav Gupta',
     "image":
         'https://firebasestorage.googleapis.com/v0/b/vihaan2021-b1b86.appspot.com/o/images%2Fjudges%2Fspeaker_arnavgupta.jpg?alt=media&token=136a8fb6-6c94-49b3-b2b1-a47e21b48ec4',
-    "designation":
-        'Founding Member - Coding Blocks\nLead Engineer, Android - Target',
+    "designation": 'Founding Member - Coding Blocksr',
+    "category": "Speaker - Innovation",
+    "linkedIn": "https://www.linkedin.com/in/arnav-gupta-b9a8b9b6/",
   },
   {
-    "name ": 'Mr. Shivaam Sharma',
+    "name": 'Mr. Shivaam Sharma',
     "image":
         'https://firebasestorage.googleapis.com/v0/b/vihaan2021-b1b86.appspot.com/o/images%2Fjudges%2Fspeaker_shivaamsharma.jpg?alt=media&token=733340bb-6512-4f6b-b24d-f34018e14b05',
     "designation": 'Founder and CEO, Trans Neuron Technologies',
+    "category": "iTrack",
+    "linkdIn": "https://in.linkedin.com/in/shivaam"
   },
   {
-    "name ": 'Mr. Utkarsh Singh',
+    "name": 'Mr. Utkarsh Singh',
     "image":
         'https://firebasestorage.googleapis.com/v0/b/vihaan2021-b1b86.appspot.com/o/images%2Fjudges%2Fjudge_utkarshsingh.jpg?alt=media&token=73ead81f-5886-454d-80d3-36df237be876',
     "designation": 'Senior Hardware Engineer, Samsung Electronics',
+    "category": "Judge",
+    "linkdIn": "https://www.linkedin.com/in/utkarsh-singh-b9a8b9b6/"
   },
   {
-    "name ": 'Ms. Drishti Agarwal',
+    "name": 'Ms. Drishti Agarwal',
     "image":
         'https://firebasestorage.googleapis.com/v0/b/vihaan2021-b1b86.appspot.com/o/images%2Fjudges%2Fjudge_drishti.jpg?alt=media&token=0d04007c-9cdb-4781-bd30-8219e24f2f88',
     "designation": 'SDE at Adobe Systems',
+    "category": "Judge",
+    "linkdIn": "https://www.linkedin.com/in/drishti-agarwal-b9a8b9b6/"
   },
   {
-    "name ": 'Ms. Ishika Hooda',
+    "name": 'Ms. Ishika Hooda',
     "image":
         'https://firebasestorage.googleapis.com/v0/b/vihaan2021-b1b86.appspot.com/o/images%2Fjudges%2Fjudge_ishika.jpg?alt=media&token=a4d364a0-ebcb-4617-921e-1fb98476d3b4',
     "designation": 'Software Developer, Atlassian',
+    "category": "Judge",
+    "linkdIn": "https://www.linkedin.com/in/ishika-hooda-b9a8b9b6/"
   },
   {
-    "name ": 'Mr. Samarth Gupta',
+    "name": 'Mr. Samarth Gupta',
     "image":
         'https://firebasestorage.googleapis.com/v0/b/vihaan2021-b1b86.appspot.com/o/images%2Fjudges%2Fjudge_samarth-1.jpg?alt=media&token=9bebe1f5-0540-444a-936e-c22d6d6896b3',
     "designation": 'Software Engineer, Microsoft\nIEEE REM Scholar',
+    "category": "Judge",
+    "linkdIn": "https://www.linkedin.com/in/samarth-gupta-b9a8b9b6/"
   },
 ];
-
-// var judgesName = [
-//   'Mr. Saurabh Jain',  DONE
-//   'Mr. Rakshit Tandon',  DONE
-//   'Mr. Sumeet Malik',  DONE
-//   'Mr. Alon Grinshpoon', DONE
-//   'Mr. Arnav Gupta',   DONE
-//   'Mr. Shivaam Sharma',  DONE
-//   'Mr. Utkarsh Singh',     DONE
-//   'Mr. Samarth Gupta',    DONE
-//   'Ms. Ishika Hooda',
-//   'Ms. Drishti Agarwal',
-//   //'Mr. Ritwik Ranjan',
-// ];
-//
-// var judgesDesignation = {
-//   'Mr. Saurabh Jain': 'Founder, Fun2Do Labs\nEx - VP, PayTM',
-//   //'Mr. Ritwik Ranjan': 'Incoming Intern, Citibank\nPostman Student Expert',
-//   'Mr. Rakshit Tandon': 'Founder & Director Hackdev Technology Pvt Ltd.',
-//   'Mr. Sumeet Malik': 'Founder at Pepcoding Education Private Limited',
-//   'Mr. Alon Grinshpoon': 'Founder & CEO at echoAR (Techstars \'19)',
-//   // 'Mr. Arnav Gupta':
-//   //     'Founding Member - Coding Blocks\nLead Engineer, Android - Target',
-//   'Mr. Arnav Gupta': 'Founding Member - Coding Blocks',
-//   'Mr. Shivaam Sharma': 'Founder and CEO, Trans Neuron Technologies',
-//   'Mr. Utkarsh Singh': 'Senior Hardware Engineer, Samsung Electronics',
-//   'Mr. Samarth Gupta': 
-//   'Ms. Ishika Hooda': 
-//   'Ms. Drishti Agarwal': 
-// };
-//
-// var judgesLinkedIn = {
-//   'Mr. Saurabh Jain': 'https://www.linkedin.com/in/saurabhskj/',
-//   //'Mr. Ritwik Ranjan': 'https://www.linkedin.com/in/ranjanritwik/',
-//   'Mr. Rakshit Tandon': 'https://in.linkedin.com/in/rakshittandon',
-//   'Mr. Sumeet Malik': 'https://in.linkedin.com/in/sumeet-malik-ab650410',
-//   'Mr. Alon Grinshpoon': 'https://www.linkedin.com/in/alongrinshpoon',
-//   'Mr. Arnav Gupta': 'https://in.linkedin.com/in/arnavgupta',
-//   'Mr. Shivaam Sharma': 'https://in.linkedin.com/in/shivaam',
-//   'Mr. Utkarsh Singh': 'https://in.linkedin.com/in/utkarshsingh1998',
-//   'Mr. Samarth Gupta': 'https://in.linkedin.com/in/samarthgupta1011',
-//   'Ms. Ishika Hooda': 'https://in.linkedin.com/in/ishikahooda',
-//   'Ms. Drishti Agarwal': 'https://in.linkedin.com/in/drishti-agarwal-211134157',
-// };
-//
-// var judgesCategory = {
-//   'Mr. Saurabh Jain': 'Speaker - FinTech',
-//   //'Mr. Ritwik Ranjan': 'Speaker - API 101',
-//   'Mr. Rakshit Tandon': 'Speaker - Security',
-//   'Mr. Sumeet Malik': 'Speaker - Education',
-//   'Mr. Alon Grinshpoon': 'Speaker - AR/VR',
-//   'Mr. Arnav Gupta': 'Speaker - Innovation',
-//   'Mr. Shivaam Sharma': 'iTrack',
-//   'Mr. Utkarsh Singh': 'Judge',
-//   'Mr. Samarth Gupta': 'Judge',
-//   'Ms. Ishika Hooda': 'Judge',
-//   'Ms. Drishti Agarwal': 'Judge',
-// };
