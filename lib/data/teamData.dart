d0565d7dc91aae8b4b0faaa7da19ff2df47ee6e1
theselfobsessedgeek <shubhamahlawatg@gmail.com>
2022-02-23 16:49:47 +0530

    team details updated in the data file

--- lib/data/teamData.dart
+++ lib/data/teamData.dart
@@ -14,6 +14,13 @@ List<Map<String, String>> ieeeDtuMembers = [
     "linkedIn": "https://www.linkedin.com/in/shubham-kumar-83b39a168/",
   },
   {
+    "name": "Chitra Singla",
+    "position": "Treasurer",
+    "image":
+        "https://firebasestorage.googleapis.com/v0/b/vihaan2021-b1b86.appspot.com/o/images%2Fleads%2Fpic%20-%20Chitra%20Singla.jpg?alt=media&token=84f07869-5c14-45cf-bf36-78a2d878d53e",
+    "linkedIn": "https://www.linkedin.com/in/chitrasingla"
+  },
+  {
     "name": "Sonal Bera",
     "position": "Chairperson\nIEEE DTU CS",
     "image":
@@ -21,24 +28,45 @@ List<Map<String, String>> ieeeDtuMembers = [
     "linkedIn": "https://www.linkedin.com/in/shubham-kumar-83b39a168/",
   },
   {
-    "name": "Manali Biswas",
-    "position": "Vice-Chairperson\nIEEE DTU",
+    "name": "Kirti Dabas",
+    "position": "Vice chairperson CS chapter -IEEE DTU",
     "image":
-        "https://firebasestorage.googleapis.com/v0/b/vihaan2021-b1b86.appspot.com/o/images%2Fteam%2Fteam_manali.jpg?alt=media&token=7cb70f99-bbac-42cd-b36e-42edd240c256",
-    "linkedIn": "https://www.linkedin.com/in/manali-biswas/",
+        "https://firebasestorage.googleapis.com/v0/b/vihaan2021-b1b86.appspot.com/o/images%2Fleads%2FIMG-20210315-WA0008%20-%20Kirti%20Dabas.jpg?alt=media&token=24405911-e045-4ceb-a538-a4712f18acbe",
+    "linkedIn": "https://www.linkedin.com/in/kirti-dabas-a11159179"
+  },
+  {
+    "name": "Shivam Shaurya",
+    "position": "Chairperson PES-IAS",
+    "image":
+        "https://firebasestorage.googleapis.com/v0/b/vihaan2021-b1b86.appspot.com/o/images%2Fleads%2FScreenshot%202022-02-08%20at%2011.44.42%20PM%20-%202K18_EE_194%20SHIVAM%20SHAURYA.png?alt=media&token=f840c8c0-3a41-4fb4-976d-f360dd9b2d53",
+    "linkedIn": "https://www.linkedin.com/in/shivam-in/"
+  },
+  // {
+  //   "name": "Manali Biswas",
+  //   "position": "WIE Chairperson\nIEEE DTU",
+  //   "image":
+  //       "https://firebasestorage.googleapis.com/v0/b/vihaan2021-b1b86.appspot.com/o/images%2Fteam%2Fteam_manali.jpg?alt=media&token=7cb70f99-bbac-42cd-b36e-42edd240c256",
+  //   "linkedIn": "https://www.linkedin.com/in/manali-biswas/",
+  // },
+  {
+    "name": "Mayank Kr Jarwal",
+    "position": "WIE Chairperson\nIEEE DTU",
+    "image":
+        "https://firebasestorage.googleapis.com/v0/b/vihaan2021-b1b86.appspot.com/o/images%2Fleads%2FIMG_20181216_215127%20-%20Mayank%20Kumar%20Jarwal.jpeg?alt=media&token=8d34e605-e98b-4935-b9bc-52eed667cb85",
+    "linkedIn": "www.linkedin.com/in/mayank-kumar-jarwal"
   },
   {
     "name": "Piyush Kr. Sahoo",
     "position": "Lead Organizer\nEvent Coordination",
     "image":
-        "https://firebasestorage.googleapis.com/v0/b/vihaan2021-b1b86.appspot.com/o/images%2Fteam%2Fteam_piyush.jpg?alt=media&token=609e9ce4-d13d-4e52-a374-9aa73e10ea26",
+        "https://firebasestorage.googleapis.com/v0/b/vihaan2021-b1b86.appspot.com/o/images%2Fleads%2FIMG_20211202_183553%20-%20Piyush%20Kumar%20Sahoo.jpg?alt=media&token=243f0a31-386b-4b0e-b043-13377c40d829",
     "linkedIn": "https://www.linkedin.com/in/piyush-kumar-sahoo-dtu/",
   },
   {
     "name": "Priyansh Tyagi",
     "position": "Lead Organizer\nEvent Coordination",
     "image":
-        "https://firebasestorage.googleapis.com/v0/b/vihaan2021-b1b86.appspot.com/o/images%2Fteam%2Fteam_priyansh.jpg?alt=media&token=96898120-9fab-40af-b725-ae9308a3427a",
+        "https://firebasestorage.googleapis.com/v0/b/vihaan2021-b1b86.appspot.com/o/images%2Fleads%2FProfile%20-%202K19_EP_075%20PRIYANSH%20TYAGI.jpeg?alt=media&token=4f8c9a09-6670-4a5f-a2ce-6179cc21e02e",
     "linkedIn": "https://www.linkedin.com/in/piyush-kumar-sahoo-dtu/",
   },
   {
@@ -48,53 +76,188 @@ List<Map<String, String>> ieeeDtuMembers = [
         "https://firebasestorage.googleapis.com/v0/b/vihaan2021-b1b86.appspot.com/o/images%2Fteam%2Fteam_hemang.jpg?alt=media&token=452c55fe-0c03-4d0c-8938-8fb578bbd78f",
     "linkedIn": "https://www.linkedin.com/in/piyush-kumar-sahoo-dtu/",
   },
-  // {
-  //   "name": "Aeshita Mathur",
-  //   "position": "Lead Organizer\nWebinar and Outreach",
-  //   "image":
-  //       "https://firebasestorage.googleapis.com/v0/b/vihaan2021-b1b86.appspot.com/o/images%2Fteam%2Fteam_piyush.jpg?alt=media&token=609e9ce4-d13d-4e52-a374-9aa73e10ea26",
-  //   "linkedIn": "https://www.linkedin.com/in/piyush-kumar-sahoo-dtu/",
-  // },
-];
+  {
+    "name": "Sakshi Panchal",
+    "position": "Lead Organizer (Wie and IR)",
+    "image":
+        "https://firebasestorage.googleapis.com/v0/b/vihaan2021-b1b86.appspot.com/o/images%2Fleads%2FIMG-20211229-WA0023%20-%202K19_EC_163%20SAKSHI%20SHAILESH%20PANCHAL.jpg?alt=media&token=bb13bd13-39a1-4a01-8f21-355aae442ef7",
+    "linkedIn": "https://www.linkedin.com/in/sakshi-panchal"
+  },
+  {
+    "name": "Sakshi Arora",
+    "position": "Lead Organizer (Outreach)",
+    "image":
+        "https://firebasestorage.googleapis.com/v0/b/vihaan2021-b1b86.appspot.com/o/images%2Fleads%2FIMG-20220208-WA0121.jpeg?alt=media&token=4f3fe9eb-8e57-4d8f-a38b-e0bf1d1ee8fa",
+    "linkedIn": "https://www.linkedin.com/in/sakshi-a-141014194/"
+  },
 
-// var ieeeMembersLinkedIn = [
-//   'https://www.linkedin.com/in/manmohan-mathur-9a7736169/',
-//   'https://www.linkedin.com/in/lavanisha-prasad-005a55197/',
-//   'https://www.linkedin.com/in/shivamkansal100/',
-//   'https://www.linkedin.com/in/akshicat/',
-//   'https://www.linkedin.com/in/ranjanritwik/',
-//   'https://www.linkedin.com/in/shubham-kumar-83b39a168/',
-//   'https://www.linkedin.com/in/thisismrids/',
-//   'https://www.linkedin.com/in/kirti-dabas-a11159179/',
-//   'https://www.linkedin.com/in/abhishek-sethi02/',
-//   'https://www.linkedin.com/in/chitrasingla',
-//   'https://www.linkedin.com/in/manali-biswas/',
-//   'https://www.linkedin.com/in/mayank-kumar-jarwal',
-//   'https://www.linkedin.com/in/shivam-in/',
-//   'https://www.linkedin.com/in/mittalnitya/',
-//   'https://www.linkedin.com/in/vaibhav-2107/',
-//   'https://www.linkedin.com/in/piyush-kumar-sahoo-dtu/',
-//   'https://www.linkedin.com/in/hemang-sinha-0485081a4/',
-//   'https://www.linkedin.com/in/priyansh-t-3336b1168/'
-// ];
-//
-// var ieeeMemberPosition = [
-//   'Chairperson\nIEEE DTU',
-//   'Vice-Chairperson\nIEEE DTU',
-//   'Chairperson\nIEEE DTU CS',
-//   'Chairperson\nIEEE DTU WIE',
-//   'Lead Organizer',
-//   'Lead Organizer',
-//   'Lead Organizer',
-//   'Lead Organizer',
-//   'Social Media Coordinator',
-//   'Social Media Coordinator',
-//   'Webinar Coordinator',
-//   'Corporate Coordinator',
-//   'PR Coordinator',
-//   'Content Team Lead',
-//   'Design Team Lead',
-//   'Design Team Lead',
-//   'Website Team Lead',
-//   'Publicity Team Lead'
-// ];
+  {
+    "name": "Aeshita Mathur",
+    "position": "Lead Organizer (Outreach)",
+    "image":
+        "https://firebasestorage.googleapis.com/v0/b/vihaan2021-b1b86.appspot.com/o/images%2Fleads%2Fmeeeee%20-%20Aeshita%20Mathur%202K19-CO-34.jpeg?alt=media&token=dc39a2f4-4330-4b7d-92f0-3803fb641953",
+    "linkedIn": "https://www.linkedin.com/in/aeshita-mathur-584863200/"
+  },
+  {
+    "name": "Supriya Koirala",
+    "position": "Lead Organizer (Outreach)",
+    "image":
+        "https://firebasestorage.googleapis.com/v0/b/vihaan2021-b1b86.appspot.com/o/images%2Fleads%2FIMG_6990_Original%20-%202K19_IT_128%20SUPRIYA%20KOIRALA.jpg?alt=media&token=6153df11-17d7-41f3-bf8c-e61599cb7bb9",
+    "linkedIn": "https://www.linkedin.com/in/supriya-koirala-430375180/"
+  },
+  {
+    "name": "Nitya Mittal",
+    "position": "Lead Organizer (Event and Membership Coordinator)",
+    "image":
+        "https://firebasestorage.googleapis.com/v0/b/vihaan2021-b1b86.appspot.com/o/images%2Fleads%2FIMG_20210828_204157_994%20-%20Nitya%20Mittal.webp?alt=media&token=2bfd8e05-9d20-4704-b0f5-2a30503d747c",
+    "linkedIn": "https://www.linkedin.com/in/mittalnitya"
+  },
+  {
+    "name": "Ritika Kanojia",
+    "position": "Lead Organizer (Content Coordinator)",
+    "image":
+        "https://firebasestorage.googleapis.com/v0/b/vihaan2021-b1b86.appspot.com/o/images%2Fleads%2FIMG_20211231_153937%20-%20Ritika%20Kanojia.jpg?alt=media&token=c866019e-f24c-4449-8cbb-f79751baadb7",
+    "linkedIn":
+        "https://firebasestorage.googleapis.com/v0/b/vihaan2021-b1b86.appspot.com/o/images%2Fleads%2FIMG_20211231_153937%20-%20Ritika%20Kanojia.jpg?alt=media&token=c866019e-f24c-4449-8cbb-f79751baadb7"
+  },
+  {
+    "name": "Pranay Khosla",
+    "position": "Lead Organizer (Logistics)",
+    "image":
+        "https://firebasestorage.googleapis.com/v0/b/vihaan2021-b1b86.appspot.com/o/images%2Fleads%2FIMG_1393%20-%202K19_EP_073%20PRANAY%20KHOSLA.jpeg?alt=media&token=c5939d00-54cc-4e8e-92dc-c1c9f76db1c9",
+    "linkedIn": "https://www.linkedin.com/in/pranay-k-50432554/"
+  },
+  {
+    "name": "Japnoor Singh",
+    "position": "Team Lead Graphic Design",
+    "image":
+        "https://firebasestorage.googleapis.com/v0/b/vihaan2021-b1b86.appspot.com/o/images%2Fleads%2FJapnoor%20Singh%20-%202k20_EC_102%20JAPNOOR%20SINGH.jpg?alt=media&token=502b2693-f9fa-4a7d-b045-90b0ed16c52b",
+    "linkedIn": "https://www.linkedin.com/in/japnoor-singh-85393a146/"
+  },
+  {
+    "name": "Shreya Sahu",
+    "position": "Team Lead WIE",
+    "image":
+        "https://firebasestorage.googleapis.com/v0/b/vihaan2021-b1b86.appspot.com/o/images%2Fleads%2FScreenshot_20220130-173710_Gallery%20-%202K20CO438%20shreya%20sahu.jpg?alt=media&token=93ba44ea-2191-4aa3-9652-58a54d13dcba",
+    "linkedIn": "https://www.linkedin.com/in/shreya-sahu-1463691bb"
+  },
+  {
+    "name": "Rishi Maheshwari",
+    "position": "Team Lead Webinar",
+    "image":
+        "https://firebasestorage.googleapis.com/v0/b/vihaan2021-b1b86.appspot.com/o/images%2Fleads%2F1643557263065%20-%20Rishi%20Maheshwari.jpg?alt=media&token=f80cd716-d20a-4240-a6d9-5dfbeef9dded",
+    "linkedIn": "https://www.linkedin.com/in/rishi-maheshwari-09199020a"
+  },
+  {
+    "name": "Nikhil Bhalla",
+    "position": "Team Lead Webinar",
+    "image":
+        "https://firebasestorage.googleapis.com/v0/b/vihaan2021-b1b86.appspot.com/o/images%2Fleads%2FWhatsApp%20Image%202022-02-07%20at%2011.30.14%20AM%20-%20Nikhil%20Bhalla.jpeg?alt=media&token=065f94c9-a222-422c-85f4-1d6f6c456e0c",
+    "linkedIn": "https://www.linkedin.com/in/nikhil-bhalla-116704206/"
+  },
+  {
+    "name": "Rishu Yadav",
+    "position": "Team Lead Networking",
+    "image":
+        "https://firebasestorage.googleapis.com/v0/b/vihaan2021-b1b86.appspot.com/o/images%2Fleads%2FRishu%20-%202K20_EE_218%20Rishu%20Yadav.jpeg?alt=media&token=1aa612d4-ae51-4995-a472-1b93cc5b27c3",
+    "linkedIn": "www.linkedin.com/in/rishu-yadav-062676202"
+  },
+  {
+    "name": "Anirudh Sharma",
+    "position": "Team Lead PR",
+    "image":
+        "https://firebasestorage.googleapis.com/v0/b/vihaan2021-b1b86.appspot.com/o/images%2Fleads%2Fxyz%20-%20Anirudh%20Sharma.jpg?alt=media&token=75d20794-8c3b-4f61-a360-a3fa2b6f943d",
+    "linkedIn": "https://www.linkedin.com/in/anirudhjs07/"
+  },
+  {
+    "name": "Deepak Kumar",
+    "position": "Team Lead PR",
+    "image":
+        "https://firebasestorage.googleapis.com/v0/b/vihaan2021-b1b86.appspot.com/o/images%2Fleads%2FPicsArt_01-13-12.24.17%20-%20Deepak%20Rai.png?alt=media&token=321db2f9-e3aa-47d0-9ccc-d7dbd47a11b7",
+    "linkedIn": "https://www.linkedin.com/in/deepak-kumar-4b194b203"
+  },
+  {
+    "name": "Chhitize Kumar",
+    "position": "Team Lead PR",
+    "image":
+        "https://firebasestorage.googleapis.com/v0/b/vihaan2021-b1b86.appspot.com/o/images%2Fleads%2Flinkedin%20-%202K20_CO_131%20Chhitize%20Kumar.jpg?alt=media&token=7e9687be-bf81-4f66-a1f7-a1dde345dd65",
+    "linkedIn": "https://www.linkedin.com/in/chhitize-kumar-b40358202/"
+  },
+  {
+    "name": "Kailash Maurya",
+    "position": "Team Lead Videography",
+    "image":
+        "https://firebasestorage.googleapis.com/v0/b/vihaan2021-b1b86.appspot.com/o/images%2Fleads%2FIMG_20211230_005306%20-%20Kailash%20Maurya.jpg?alt=media&token=eadfd43c-6312-4d29-8859-ee1380155b7d",
+    "linkedIn": "https://www.linkedin.com/mwlite/in/kailash-maurya-032898208"
+  },
+  {
+    "name": "Sidakpreet Singh",
+    "position": "Team Lead Videography",
+    "image":
+        "https://firebasestorage.googleapis.com/v0/b/vihaan2021-b1b86.appspot.com/o/images%2Fleads%2FIMG_20211205_164341__01%20-%20Sidakpreet%20Singh.jpg?alt=media&token=2435aaa7-3acc-47b3-8662-7aba88061d29",
+    "linkedIn": "https://www.linkedin.com/in/sidakpreet-singh-031a52228/"
+  },
+  {
+    "name": "Kshitij Singh",
+    "position": "Design Team Lead",
+    "image":
+        "https://firebasestorage.googleapis.com/v0/b/vihaan2021-b1b86.appspot.com/o/images%2Fleads%2FIMG_0023%20-%2035_AE%20KSHITIJ%20SINGH.JPG?alt=media&token=ba76da48-3ba7-4c07-917b-d5091aadd20e",
+    "linkedIn": "https://www.linkedin.com/in/kshitij-singh-29b23b201/"
+  },
+  {
+    "name": "Rishi Raj Thakur",
+    "position": "Team lead Networking",
+    "image":
+        "https://firebasestorage.googleapis.com/v0/b/vihaan2021-b1b86.appspot.com/o/images%2Fleads%2FIMG_20220130_181330%20-%202K20-ME-216%20Rishi%20Raj%20Thakur.jpg?alt=media&token=c5d16c1f-8adc-4985-b7ec-df2acc872a96",
+    "linkedIn": "https://www.linkedin.com/in/rishi-raj-thakur-24b3a0213"
+  },
+  {
+    "name": "Dhruv Talan",
+    "position": "Team Lead Networking",
+    "image":
+        "https://firebasestorage.googleapis.com/v0/b/vihaan2021-b1b86.appspot.com/o/images%2Fleads%2FDhruv%20Talan%20networking_vihaan_team%20lead%20-%20Dhruv%20Talan.jpeg?alt=media&token=54ad9e3f-2cce-42c3-9a5a-c3818d297e27",
+    "linkedIn": "www.linkedin.com/in/dhruv-talan-852799218"
+  },
+  {
+    "name": "Ansh Anurag",
+    "position": "Event Team Lead",
+    "image":
+        "https://firebasestorage.googleapis.com/v0/b/vihaan2021-b1b86.appspot.com/o/images%2Fleads%2F156020173_255003010076483_6052928117465092032_n%20-%20IT-139%20Shubham%20Ahlawat.jpg?alt=media&token=25aece4a-19da-4ab7-b5a5-a90995b362da",
+    "linkedIn": "https://www.linkedin.com/in/ansh-anurag-92678416b/"
+  },
+  {
+    "name": "Saujanya Sood",
+    "position": "Event Team Lead",
+    "image":
+        "https://firebasestorage.googleapis.com/v0/b/vihaan2021-b1b86.appspot.com/o/images%2Fleads%2F41ACE175-F044-478B-B9A8-94F4224D14B4%20-%20IT-131%20Saujanya%20Sood.jpeg?alt=media&token=b98c24ae-2a66-4207-8f07-d6ff0d0c0b6b",
+    "linkedIn": "https://www.linkedin.com/in/saujanyasood"
+  },
+  {
+    "name": "Anika Passi",
+    "position": "Event Team Lead ",
+    "image":
+        "https://firebasestorage.googleapis.com/v0/b/vihaan2021-b1b86.appspot.com/o/images%2Fleads%2F11190AC7-DB12-4826-BACA-36EE65DB12BF%20-%202K20%20EN%2014%20Anika%20Passi.jpeg?alt=media&token=00e49f37-f0ce-4aca-9bd2-5b24f220a28d",
+    "linkedIn": "https://www.linkedin.com/in/anikapassi"
+  },
+  {
+    "name": "Arya Pandey",
+    "position": "Content Team Lead",
+    "image":
+        "https://firebasestorage.googleapis.com/v0/b/vihaan2021-b1b86.appspot.com/o/images%2Fleads%2FWhatsApp%20Image%202022-01-30%20at%2017.18.51%20-%20Arya%20Pandey%2031.jpeg?alt=media&token=eef9c71e-bbfb-4f89-805b-983c3a3f7389",
+    "linkedIn": "https://www.linkedin.com/in/arya-pandey-5340651b8/"
+  },
+  {
+    "name": "Aditya Singh Mann",
+    "position": "Content Team Lead",
+    "image":
+        "https://firebasestorage.googleapis.com/v0/b/vihaan2021-b1b86.appspot.com/o/images%2Fleads%2FScreenshot_20210403-212431%20-%20Aditya%20Singh%20Mann.jpg?alt=media&token=6a97abc2-b70a-4123-9954-fc8b55f92210",
+    "linkedIn": ""
+  },
+  {
+    "name": "Shubham Ahlawat",
+    "position": "Website Coordinator",
+    "image":
+        "https://firebasestorage.googleapis.com/v0/b/vihaan2021-b1b86.appspot.com/o/images%2Fleads%2FWhatsApp%20Image%202022-01-25%20at%2000.25.45.jpeg?alt=media&token=16f0f569-7632-40c1-9515-f4c3ae9ee4fc",
+    "linkedIn": "https://www.linkedin.com/in/shubham-ahlawat-55432336/",
+  },
+];
